9e6c12e2dacb74bc58f8fba23bec472df20b0626
Gerardo Bort <gerardobort@gmail.com>
2014-02-12 21:05:18 -0300

    general refactor - behavior more than acceptable

--- public/ps13/main.js
+++ public/ps13/main.js
@@ -8,6 +8,7 @@ function $(id) { return document.getElementById(id); }
 var video = $('video'),
     videoSource = 'gopro3d/GoPro 3D  Winter X Games 2011 Highlights.sd.mp4',
     canvas = $('canvas1'),
+    canvasDepth = $('canvas2'),
     canvasFrame = new CanvasFrame(canvas);
 
 canvas.onclick = function () {
@@ -35,6 +36,10 @@ function CanvasFrame(canvas) {
 
     this.context = canvas.getContext('2d');
 
+    canvasDepth.width = CANVAS_WIDTH/2;
+    canvasDepth.height = CANVAS_HEIGHT;
+    this.depthContext = canvasDepth.getContext('2d');
+
     // initialize variables
     this.buffer = this.context.createImageData(CANVAS_WIDTH, CANVAS_HEIGHT);
 
@@ -61,12 +66,15 @@ CanvasFrame.prototype.transform = function() {
 
     var MOTION_COLOR_THRESHOLD = 80,
         GRID_FACTOR = 1,
-        RIGHT_SCANNING_ANGLE = 15, // deg
+        RIGHT_SCANNING_ANGLE = -1 * 30, // deg
         SCAN_MAX_OFFSET = GRID_FACTOR*20,
         i = l = x = y = 0, w = CANVAS_WIDTH, h = CANVAS_HEIGHT,
         fscan, d, m = Math.tan(Math.PI/(180/RIGHT_SCANNING_ANGLE));
 
-    var dx, j, xr, yr, cl, cr, k, depth, colorDepth;
+    var dx, j, xr, yr, cl, cr, k, depth, colorDepth, offsetFrom, offsetTo;
+
+    var ctx = this.context;
+
     // iterate through the entire buffer
     for (i = 0; i < len; i += 4) {
         x = (i/4) % w;
@@ -74,29 +82,37 @@ CanvasFrame.prototype.transform = function() {
         if (x < CANVAS_WIDTH/2) {
             y = parseInt((i/4) / w);
             if (!(x % GRID_FACTOR) && !(y % GRID_FACTOR)) {
-                d = y - m*(x); // shifted to the right video stream
-                fscan = function (xi) { return (m*xi + d); };
+                d = y - m*(x + w/2); // shifted to the right video stream
+                fscan = function (xi) { return /*h -*/ (m*xi + d); };
+
+                // default is full depth
+                newpx[i+0] = 0;
+                newpx[i+1] = 0;
+                newpx[i+2] = 0;
+                newpx[i+3] = 255;
 
                 // pick the left side pixel color
                 cl = [videopx[i+0], videopx[i+1], videopx[i+2]];
-                for (dx = - SCAN_MAX_OFFSET; dx < SCAN_MAX_OFFSET; dx+=2) {
+                for (dx = SCAN_MAX_OFFSET; dx > -SCAN_MAX_OFFSET; dx-=3) {
                     xr = w/2 + x + dx;
-                    yr = parseInt(fscan(x), 10);
+                    yr = parseInt(fscan(xr), 10);
                     if (0 === dx || xr < w/2 || xr > w || yr < 0 || yr > h) continue;
                     j = (yr*w + xr)*4;
 
                     // pick the right side scanning pixel color
                     cr = [videopx[j+0], videopx[j+1], videopx[j+2]];
 
+
                     // if matches then draw the depthmap at the right
                     if (distance3(cl, cr, 0) < MOTION_COLOR_THRESHOLD) {
-                        //k = (y*w + w/2 + x)*4;
                         depth = 1/(2*SCAN_MAX_OFFSET) * (dx + SCAN_MAX_OFFSET); // estimate depth 0 to 1 (higher is deeper)
+
                         colorDepth = parseInt(depth*255, 10);
+
                         newpx[i+0] = colorDepth;
-                        newpx[i+1] = 0;
-                        newpx[i+2] = 0;
-                        //newpx[k+3] = colorDepth;
+                        newpx[i+1] = colorDepth;
+                        newpx[i+2] = colorDepth;
+                        newpx[i+3] = 255;
                         break;
                     }
 
@@ -104,7 +120,7 @@ CanvasFrame.prototype.transform = function() {
             }
         }
     }
-    this.context.putImageData(newdata, 0, 0);
+    this.depthContext.putImageData(newdata, 0, 0);
 
 };
 
