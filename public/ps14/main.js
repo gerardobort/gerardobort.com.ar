a276f727d19a5a261f8bdeb58b42ed86079fec41
Gerardo Bort <gerardobort@gmail.com>
2014-02-20 12:05:04 -0800

    latest changes

--- public/ps14/main.js
+++ public/ps14/main.js
@@ -23,7 +23,7 @@ video.addEventListener('loadedmetadata', function () {
             demo = {
                 OFFSET_X: 1,
                 OFFSET_Y: -4,
-                COLOR_THRESHOLD: 60,
+                BALANCE_THRESHOLD: 10,
                 ERROR_TOLERANCE: 200,
                 DEPTH_STEP: 5,
                 GRID_FACTOR: 1,
@@ -51,9 +51,9 @@ video.addEventListener('loadedmetadata', function () {
             gui = new dat.GUI({ width: 400 });
             gui.add(demo, 'OFFSET_X', - CANVAS_WIDTH, CANVAS_WIDTH).step(1);
             gui.add(demo, 'OFFSET_Y', - CANVAS_HEIGHT/2, CANVAS_HEIGHT/2).step(1);
-            gui.add(demo, 'COLOR_THRESHOLD', 0, 255).step(1);
+            gui.add(demo, 'BALANCE_THRESHOLD', 0, 255).step(1);
             gui.add(demo, 'ERROR_TOLERANCE', 0, 255).step(1);
-            gui.add(demo, 'DEPTH_STEP', 1, 10).step(1);
+            gui.add(demo, 'DEPTH_STEP', 1, 40).step(1);
             gui.add(demo, 'GRID_FACTOR', 1, 40).step(1);
             gui.add(demo, 'STOCASTIC_RATIO', 0, 1).step(0.0001);
             gui.add(demo, 'DEPTH_SATURATION', 0, 10).step(0.0001);
@@ -127,6 +127,7 @@ CanvasFrame.prototype.transform = function() {
     var yellow = [255, 240, 42]; // (CMYK=0,0,100,0)
     var e = demo.ERROR_TOLERANCE;
     var step = demo.DEPTH_STEP;
+    var righthandY = {};
 
 
     var ctx = this.context;
@@ -136,6 +137,59 @@ CanvasFrame.prototype.transform = function() {
 
         if (0 === x) depth = 255;
         x = (i/4) % w;
+        // only with the left side video...
+        if (x < CANVAS_WIDTH/2) {
+            y = parseInt((i/4) / w);
+            if (!(x % demo.GRID_FACTOR) && !(y % demo.GRID_FACTOR) && Math.random() > demo.STOCASTIC_RATIO) {
+
+                xr = w/2 + x + demo.OFFSET_X;
+                yr = y + demo.OFFSET_Y;
+                j = (yr*w + xr)*4;
+                k = (y*w + w/2 -1)*4;
+
+                distance = parseInt(Math.sqrt(
+                    (videopx[i]-videopx[j])*(videopx[i]-videopx[j])
+                    + (videopx[i+1]-videopx[j+1])*(videopx[i+1]-videopx[j+1])
+                    + (videopx[i+2]-videopx[j+2])*(videopx[i+2]-videopx[j+2])
+                ), 10);
+                
+                cl = [videopx[j], distance, videopx[i]];
+                if (distance3(cl, cyan, 0) < e) {
+                    depth -= step;
+                } else if (distance3(cl, yellow, 0) < e) {
+                    depth += step;
+                }
+
+                // draw depth canvas buffer
+                if (demo.SHOW_DEPTH) {
+                    newpx[i+0] = depth;
+                    newpx[i+1] = depth;
+                    newpx[i+2] = depth;
+                } else {
+                    newpx[i+0] = videopx[j];
+                    newpx[i+1] = distance;
+                    newpx[i+2] = videopx[i];
+                }
+                
+                if (x === w/2-1) {
+                    righthandY[y] = depth;
+                }
+                
+            } else {
+                newpx[i+0] = 0;
+                newpx[i+1] = 0;
+                newpx[i+2] = 0;
+                newpx[i+3] = 255;
+            }
+        }
+    }
+
+
+    // iterate through the entire buffer
+    for (i = len-4; i > -1; i -= 4) {
+
+        if (w/2 === x) depth = 255;
+        x = Math.floor((i/4) % w, 10) +1;
         // only with the left side video...
         if (x < CANVAS_WIDTH/2) {
             y = parseInt((i/4) / w);
@@ -154,9 +208,9 @@ CanvasFrame.prototype.transform = function() {
                 
                 cl = [videopx[j], distance, videopx[i]];
                 if (distance3(cl, cyan, 0) < e) {
-                    depth -= step;
-                } else if (distance3(cl, yellow, 0) < e) {
                     depth += step;
+                } else if (distance3(cl, yellow, 0) < e) {
+                    depth -= step;
                 }
 
                 // draw depth canvas buffer
